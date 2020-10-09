7414436c6a8004abbc784f4ccac4aac4644802b5
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-10-09 14:06:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,5 +16,18 @@ console.log("ml5 version",ml5.version);
 classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XPtvlUBIT/model.json",modelloaded)
 function modelloaded(){
     console.log("Model Loaded");
-
+}
+function identify() {
+    img=document.getElementById("captured_image");
+    classifier.classify(img,getResult);
+}
+function getResult(error,results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("member_name").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(4);
+    }
 }
